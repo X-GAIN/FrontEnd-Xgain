5a7d7fa8f21c54d5b60cc21def6190f17976ec97
João A. Passos <112397879+joaoapassos@users.noreply.github.com>
2023-09-27 20:03:30 -0300

    0.2.0
    Criação e estilização das telas sobre e contato;
    Algumas correções no footer e no iframe de login/cadastro;

--- Js/loading.js
+++ Js/loading.js
@@ -7,5 +7,5 @@ function preLoading(){
     setTimeout(() => {
         loading.style.display = 'none'
         document.querySelector('#body').style.overflow = 'auto';
-    }, 500);
+    }, 700);
 }
--- Js/main.js
+++ Js/main.js
@@ -6,6 +6,19 @@ sr.reveal('.section-produtos-destaque-home');
 sr.reveal('.section-sobre-home');
 sr.reveal('.section-banner-cadastro');
 
+sr.reveal('.section-banner-titulo-main');
+sr.reveal('.area-sobre-nos');
+sr.reveal('.area-como');
+sr.reveal('.area-desafios');
+sr.reveal('.area-bem-vindo');
+
+
+sr.reveal('.section-forms-contato');
+sr.reveal('.area-info');
+sr.reveal('.area-redes-sociais');
+
+
+
 //Banner
 
 // document.getElementById('btn-setinha-banner-D').addEventListener('click', () => {
@@ -16,3 +29,8 @@ sr.reveal('.section-banner-cadastro');
 //     const scrollPosition = document.getElementById('banners-img').clientWidth;
 //     document.getElementById('banners-img').scrollLeft -= scrollPosition;
 // });
+
+
+document.getElementById('exit-aviso-section-contato').addEventListener('click', () => {
+    document.getElementById('area-aviso-contato').style.visibility = "hidden";
+})
