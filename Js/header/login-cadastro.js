620b1f008265defff39c9b8d1bfffb50c4961c86
João A. Passos <112397879+joaoapassos@users.noreply.github.com>
2023-10-23 20:23:06 -0300

    Versão 0.5.2
    Função de verificação pronta;
    Função de modal que notifica o envio do orçarmento

--- Js/header/login-cadastro.js
+++ Js/header/login-cadastro.js
@@ -13,30 +13,29 @@ document.getElementById('exit-login-section').addEventListener('click', () => {
     document.getElementById('section-login-cadastro').style.display='none';
     document.getElementById('my-header').style.zIndex='70';
     document.getElementById('iframe-login-cadastro').style.display='none';
-    document.getElementById('iframe-login-cadastro').src = '../iframe/login.html';
 });
 
 function fecharTudo(){
     document.getElementById('body').style.overflowY='auto';
     document.getElementById('section-login-cadastro').style.display='none';
     document.getElementById('my-header').style.zIndex='70';
-    document.getElementById('iframe-login-cadastro').style.display='none';
-    document.getElementById('iframe-login-cadastro').src = '../iframe/login.html';
+    document.getElementById('iframe-login').style.display='none';
+    document.getElementById('iframe-cadastro').style.display='none';
 };
 
 function abrirSectionCadastro(){
     document.getElementById('body').style.overflowY='hidden';
     document.getElementById('section-login-cadastro').style.display='block';
-    document.getElementById('iframe-login-cadastro').style.display='block';
+    document.getElementById('iframe-cadastro').style.display='block';
     document.getElementById('body').style.overflowY='hidden';
-    document.getElementById('iframe-login-cadastro').src = '../iframe/cadastro.html';
+    document.getElementById('iframe-login').style.display='none';
 };
 function abrirSectionLogin(){
     document.getElementById('body').style.overflowY='hidden';
     document.getElementById('section-login-cadastro').style.display='block';
-    document.getElementById('iframe-login-cadastro').style.display='block';
+    document.getElementById('iframe-login').style.display='block';
     document.getElementById('body').style.overflowY='hidden';
-    document.getElementById('iframe-login-cadastro').src = '../iframe/login.html';
+    document.getElementById('iframe-cadastro').style.display='none';
 };
 
 document.getElementById('exit-aviso-section').addEventListener('click', fecharAviso)
--- Js/main-fechar-orcamento.js
+++ Js/main-fechar-orcamento.js
@@ -25,7 +25,7 @@ document.getElementById('exit-aviso-section-orcamento').addEventListener('click'
 });
 
 (function () {
-    const cep = document.querySelector("input[name=cep]");
+    const cep = document.querySelector("input[name=cepOrcamento]");
 
     cep.addEventListener("blur", (e) => {
     const value = cep.value.replace(/[^0-9]+/, "");
@@ -42,4 +42,68 @@ document.getElementById('exit-aviso-section-orcamento').addEventListener('click'
 });
 })();
 
+var envio = false;
+function verificarFormsOrcamento(){
+    const todosInputs = document.querySelectorAll('.inputs-orcamento');
+        const areaAviso = document.getElementById('area-aviso-orcamento');
+        const aviso = document.getElementById('aviso');
+        const motivo = document.getElementById('motivo');
+        motivo.innerHTML = '--ERRO--' 
+        let todosPreenchidos = true;
+        const email = document.querySelector('input[name=emailOrcamento]');
+        const telefone = document.querySelector('input[name=telefoneOrcamento]');
+        let telefoneValue = telefone.value;
+        telefoneValue = telefoneValue.replace(/[^\d]/g, '');
+        const cep = document.querySelector('input[name=cepOrcamento]');
+        let cepValue = cep.value;
+        cepValue = cepValue.replace(/[^\d]/g, '');
 
+
+        todosInputs.forEach(input => {
+            if (input.value.trim() === '') {
+                todosPreenchidos = false;
+            }
+        });
+
+        if (!todosPreenchidos) {
+            areaAviso.style.display = 'flex';
+            aviso.innerHTML = 'Preencha todos os campos';
+        }
+        else if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+            areaAviso.style.display = 'flex';
+            aviso.innerHTML = 'Email incorreto';
+        }
+
+        else if(telefoneValue === ''){
+            areaAviso.style.display = 'flex';
+            aviso.innerHTML = 'Telefone inválido';
+        }
+        else if(!(telefoneValue.length >= 10 && telefoneValue.length <= 11)){
+            areaAviso.style.display = 'flex';
+            aviso.innerHTML = 'Telefone inválido';
+        }
+        else if(cepValue === ''){
+            areaAviso.style.display = 'flex';
+            aviso.innerHTML = 'CEP inválido';
+        }
+        else if(!(cepValue.length === 8)){
+            areaAviso.style.display = 'flex';
+            aviso.innerHTML = 'CEP inválido';
+        }
+        else{
+            const formulario = document.querySelector('form');
+            formulario.submit();
+            envio = true;
+            window.location.href = `fechar-orcamento.html?envio=${envio}`;
+        }
+}
+const urlParams = new URLSearchParams(window.location.search);
+envio = urlParams.get('envio');
+console.log(envio);
+
+if(envio){
+    const areaAviso = document.getElementById('area-aviso-orcamento');
+    areaAviso.style.display = 'flex';
+    envio = null;
+    console.log(envio);
+}
