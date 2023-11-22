0744142f61c1cfed8704317f7024faf189cdc88c
João A. Passos <112397879+joaoapassos@users.noreply.github.com>
2023-11-22 16:44:20 -0300

    Versão 0.6.5
    Adição da função que altera o titulo e o title da pagina de categorias;
    Adição função que remove a seleção de filtro;

--- Js/main-resultados.js
+++ Js/main-resultados.js
@@ -1,75 +1,75 @@
-document.getElementById('icon-filter').addEventListener('click', ()=>{
-    document.getElementById('section-filtro').classList.add('sectionFiltroFechado');
-})
+// document.getElementById('icon-filter').addEventListener('click', ()=>{
+//     document.getElementById('section-filtro').classList.add('sectionFiltroFechado');
+// })
 
 document.getElementById('abrir-itens-filtro-categoria-cardio').addEventListener('click', ()=>{
 
     if(document.getElementById('itens-filtro-categoria-cardio').style.display === 'block'){
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none'
-        document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-180deg)'
+        document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-90deg)'
     }
     else{
     document.getElementById('itens-filtro-categoria-cardio').style.display = 'block';
-    document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-90deg)'
+    document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(0deg)'
     }
 })
 document.getElementById('abrir-itens-filtro-categoria-musculacao').addEventListener('click', ()=>{
 
     if(document.getElementById('itens-filtro-categoria-musculação').style.display === 'block'){
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none'
-        document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-180deg)'
+        document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-90deg)'
     }
     else{
     document.getElementById('itens-filtro-categoria-musculação').style.display = 'block';
-    document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-90deg)'
+    document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(0deg)'
     }
 })
 document.getElementById('abrir-itens-filtro-categoria-acessorios').addEventListener('click', ()=>{144
 
     if(document.getElementById('itens-filtro-categoria-acessorios').style.display === 'block'){
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none'
-        document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-180deg)'
+        document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-90deg)'
     }
     else{
     document.getElementById('itens-filtro-categoria-acessorios').style.display = 'block';
-    document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-90deg)'
+    document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(0deg)'
     }
 })
 
 window.addEventListener('resize', ()=>{
     if(window.innerWidth < 769){
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
-        document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-180deg)';
+        document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-90deg)';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
-        document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-180deg)';
+        document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-90deg)';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
-        document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-180deg)';
+        document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-90deg)';
     }
     else{
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'block';
-        document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-90deg)';
+        document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(0deg)';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'block';
-        document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-90deg)';
+        document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(0deg)';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'block';
-        document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-90deg)';
+        document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(0deg)';
     }
 })
 
 if(window.innerWidth < 769){
     document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
-    document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-180deg)';
+    document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-90deg)';
     document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
-    document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-180deg)';
+    document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-90deg)';
     document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
-    document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-180deg)';
+    document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-90deg)';
 }
 else{
     document.getElementById('itens-filtro-categoria-cardio').style.display = 'block';
-    document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(-90deg)';
+    document.getElementById('abrir-itens-filtro-categoria-cardio').style.transform = 'rotate(0deg)';
     document.getElementById('itens-filtro-categoria-musculação').style.display = 'block';
-    document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(-90deg)';
+    document.getElementById('abrir-itens-filtro-categoria-musculacao').style.transform = 'rotate(0deg)';
     document.getElementById('itens-filtro-categoria-acessorios').style.display = 'block';
-    document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(-90deg)';
+    document.getElementById('abrir-itens-filtro-categoria-acessorios').style.transform = 'rotate(0deg)';
 };
 
 
@@ -141,25 +141,55 @@ function categoriaFiltros(){
         document.getElementById('filtro-cardio').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Cárdio | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Cárdio';
         cardioTag = null;
     }
     else if(musculacaoTag){
         document.getElementById('filtro-musculacao').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Musculação | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Musculação';
         musculacaoTag = null;
     }
     else if(acessoriosTag){
         document.getElementById('filtro-acessorios').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Acessórios | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Acessórios';
         acessoriosTag = null;
+    }
+
+    const urlComFiltro = window.location.href;
+    document.getElementById('icon-filter').title = 'Categorias';
+    if(urlComFiltro.includes('&')){
+        console.log(urlComFiltro)
+        document.getElementById('icon-filter').title = 'Limpar seleção';
+        document.getElementById('icon-filter').addEventListener('click', () => {
+            let recortarFiltro = urlComFiltro.split('&');
+            let urlSemFiltro = recortarFiltro[0];
+            window.location.href = urlSemFiltro;
+            console.log(urlComFiltro)
+        });
     }
 }
 
 categoriaFiltros()
 
-var esteiraTag = false, escadaTag = false, bikeTag = false, abdomenTag = false, ombroTag = false, peitoTag = false, pernaTag = false, utilitariosTag = false, equipamentosTag = false, outrosTag = false;
+var esteiraTag = false, 
+escadaTag = false, 
+bikeTag = false, 
+abdomenTag = false, 
+bracoTag = false,
+costasTag = false,
+ombroTag = false, 
+peitoTag = false, 
+pernaTag = false, 
+utilitariosTag = false, 
+equipamentosTag = false, 
+outrosTag = false;
 function subCategoriaFiltros(){
     const subCategorias = document.querySelectorAll('.subFiltro')
 
@@ -170,6 +200,7 @@ function subCategoriaFiltros(){
             const bike = subCategoria.target.classList.contains('bike');
             const abdomen = subCategoria.target.classList.contains('abdomen');
             const braco = subCategoria.target.classList.contains('braco');
+            const costas = subCategoria.target.classList.contains('costas');
             const ombro = subCategoria.target.classList.contains('ombro');
             const peito = subCategoria.target.classList.contains('peito');
             const perna = subCategoria.target.classList.contains('perna');
@@ -249,6 +280,21 @@ function subCategoriaFiltros(){
                 }
                 else{
                     var atualizarUrl = window.location.href + `&braco=${bracoTag}`;
+                    window.location.href = atualizarUrl;
+                }
+                console.log(atualizarUrl);
+            }
+            else if(costas){
+                costasTag = true;
+                const url = window.location.href;
+                if(url.includes('&')){
+                    var limparUrl = url.split('&');
+                    var urlLimpa = limparUrl[0];
+                    var atualizarUrl = urlLimpa + `&costas=${costasTag}`;
+                    window.location.href = atualizarUrl;
+                }
+                else{
+                    var atualizarUrl = window.location.href + `&costas=${costasTag}`;
                     window.location.href = atualizarUrl;
                 }
                 console.log(atualizarUrl);
@@ -358,6 +404,7 @@ function subCategoriaFiltros(){
     bikeTag = urlParams.get('bike');
     abdomenTag = urlParams.get('abdomen');
     bracoTag = urlParams.get('braco');
+    costasTag = urlParams.get('costas');
     ombroTag = urlParams.get('ombro');
     peitoTag = urlParams.get('peito');
     pernaTag = urlParams.get('perna');
@@ -371,6 +418,8 @@ function subCategoriaFiltros(){
         document.getElementById('subFiltro-esteira').style.borderBottomColor = '#992d2d';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Esteira | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Esteira';
         esteiraTag = null;
     }
     else if(escadaTag){
@@ -379,6 +428,8 @@ function subCategoriaFiltros(){
         document.getElementById('subFiltro-escada').style.borderBottomColor = '#992d2d';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Escada | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Escada';
         escadaTag = null;
     }
     else if (bikeTag) {
@@ -387,6 +438,8 @@ function subCategoriaFiltros(){
     document.getElementById('subFiltro-bike').style.borderBottomColor = '#992d2d';
     document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
     document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+    document.getElementById('titlePaginacategoria').innerHTML = 'Bike | XGAIN';
+    document.getElementById('categoriaTitle').innerHTML = 'Bike';
     bikeTag = null;
     }
 
@@ -396,6 +449,8 @@ function subCategoriaFiltros(){
         document.getElementById('subFiltro-abdomen').style.borderBottomColor = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Abdômen | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Abdômen';
         abdomenTag = null;
     }
     
@@ -405,7 +460,19 @@ function subCategoriaFiltros(){
         document.getElementById('subFiltro-braco').style.borderBottomColor = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Braço | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Braço';
         bracoTag = null;
+    }
+    else if(costasTag){
+        document.getElementById('subFiltro-costas').style.color = '#992d2d';
+        document.getElementById('filtro-musculacao').style.color = '#992d2d';
+        document.getElementById('subFiltro-costas').style.borderBottomColor = '#992d2d';
+        document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
+        document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Costas | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Costas';
+        costasTag = null;
     }
     
     else if(ombroTag){
@@ -414,6 +481,8 @@ function subCategoriaFiltros(){
         document.getElementById('filtro-musculacao').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Ombro | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Ombro';
         ombroTag = null;
     }
     else if(peitoTag){
@@ -422,6 +491,8 @@ function subCategoriaFiltros(){
         document.getElementById('filtro-musculacao').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Peito | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Peito';
         peitoTag = null;
     }
     
@@ -431,6 +502,8 @@ function subCategoriaFiltros(){
         document.getElementById('filtro-musculacao').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-acessorios').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Perna | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Perna';
         pernaTag = null;
     }
     
@@ -440,6 +513,8 @@ function subCategoriaFiltros(){
         document.getElementById('filtro-acessorios').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Utilitários | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Utilitários';
         utilitariosTag = null;
     }
     
@@ -449,6 +524,8 @@ function subCategoriaFiltros(){
         document.getElementById('filtro-acessorios').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Equipamentos | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Equipamentos';
         equipamentosTag = null;
     }
     
@@ -458,6 +535,8 @@ function subCategoriaFiltros(){
         document.getElementById('filtro-acessorios').style.color = '#992d2d';
         document.getElementById('itens-filtro-categoria-cardio').style.display = 'none';
         document.getElementById('itens-filtro-categoria-musculação').style.display = 'none';
+        document.getElementById('titlePaginacategoria').innerHTML = 'Outros | XGAIN';
+        document.getElementById('categoriaTitle').innerHTML = 'Outros';
         outrosTag = null;
     }
     
@@ -483,3 +562,8 @@ function alterarTituloDaPesquisa(){
 alterarTituloDaPesquisa();
 
 
+// function (){
+//     var filtroLimpar = document.getElementById('icon-filter');
+//     
+    
+// }
