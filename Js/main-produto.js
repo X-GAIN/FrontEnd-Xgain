1de22635fbf8770f25484bab502a43ec4420dc1c
João A. Passos <112397879+joaoapassos@users.noreply.github.com>
2023-11-22 19:40:44 -0300

    Versão 0.6.5.3
    Adição de um possível iframe do carrinho;
    Adição de uma basica função dos btns de mais e menos na tela produto;

--- Js/main-produto.js
+++ Js/main-produto.js
@@ -9,17 +9,6 @@ document.getElementById('icon-3d2').addEventListener('click', () => {
     document.getElementById('section-img3d').style.display = "flex";
 });
 
-// let produto1 = document.getElementById('produto-1'), produto2 = document.getElementById('produto-2'), produto3 = document.getElementById('produto-3');
-// produto1.addEventListener('click', () => {
-//     document.getElementById('img-principal').src = produto1.src;
-// });
-// produto2.addEventListener('click', () => {
-//     document.getElementById('img-principal').src = produto2.src;
-// });
-// produto3.addEventListener('click', () => {
-//     document.getElementById('img-principal').src = produto3.src;
-// });
-
 const imagensProdutos = document.querySelectorAll('.img-produto-galery');
 
 imagensProdutos.forEach(imagemProduto => {
@@ -27,3 +16,12 @@ imagensProdutos.forEach(imagemProduto => {
         document.getElementById('img-principal').src = imagemProduto.src;
     });
 });
+
+function maisOuMenos(valor){
+    const numero = document.getElementById('numero-produto');
+    let contador = parseInt(numero.textContent);
+
+    contador = Math.max(1, contador + valor);
+    
+    numero.textContent = contador;
+}
